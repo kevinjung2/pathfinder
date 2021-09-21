848c8380d4b6b574c84ad266a7458b6968a70bf4
Kevin Jung <kev.jung.ii@gmail.com>
2021-09-20 22:35:43 -0500

    adds state for basic options

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [gridSize, changeGridSize] = useState(100)
+  const [algorithm, changeAlgorithm] = useState("dykstras")
+  const [wall, toggleWall] = useState(false)
+
   return (
     <div className="App">
-      
+
     </div>
   );
 }
